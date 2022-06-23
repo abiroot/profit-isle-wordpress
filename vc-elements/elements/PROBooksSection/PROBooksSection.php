37f15fa698cc273989f1a81802ee298419770a6c
AkikiCharbel <charbelakiki@abiroot.com>
2022-06-23 14:01:50 +0300

    book section done

--- vc-elements/elements/PROBooksSection/PROBooksSection.php
+++ vc-elements/elements/PROBooksSection/PROBooksSection.php
@@ -20,44 +20,46 @@ if (!class_exists('PROBooksSection')) {
                     'category' => __('Profit-isle Elements', 'profit-isle'),
                     'params' => [
                         [
-                            'heading' => 'Title',
+                            'heading' => 'Book Title',
                             'type' => 'textfield',
-                            'param_name' => 'section_title',
-                            'group' => 'General Info',
+                            'param_name' => 'first_book_title',
+                            'group' => 'First Download',
                             'value' => '',
                         ],
                         [
-                            'heading' => 'Title',
+                            'heading' => 'Book Title',
                             'type' => 'textfield',
-                            'param_name' => 'section_title',
-                            'group' => 'General Info',
+                            'param_name' => 'second_book_title',
+                            'group' => 'Second Download',
                             'value' => '',
                         ],
                         [
-                            'heading' => 'Title',
+                            'heading' => 'Link',
                             'type' => 'textfield',
-                            'param_name' => 'section_title',
-                            'group' => 'General Info',
+                            'param_name' => 'first_book_link',
+                            'group' => 'First Download',
                             'value' => '',
                         ],
                         [
-                            'heading' => 'Title',
+                            'heading' => 'Link',
                             'type' => 'textfield',
-                            'param_name' => 'section_title',
-                            'group' => 'General Info',
+                            'param_name' => 'second_book_link',
+                            'group' => 'Second Download',
                             'value' => '',
                         ],
                         [
-                            'heading' => 'Testimonial Image',
+                            'heading' => 'Book Image',
                             'type' => 'attach_image',
-                            'param_name' => 'testimonial_image',
+                            'param_name' => 'first_book_image',
                             'value' => '',
+                            'group' => 'First Download',
                         ],
                         [
-                            'heading' => 'Testimonial Image',
+                            'heading' => 'Book Image',
                             'type' => 'attach_image',
-                            'param_name' => 'testimonial_image',
+                            'param_name' => 'second_book_image',
                             'value' => '',
+                            'group' => 'Second Download',
                         ],
                     ],
                 ]
@@ -74,8 +76,18 @@ if (!class_exists('PROBooksSection')) {
             wp_enqueue_script('pro_books_section-script', get_template_directory_uri() .
                 "/vc-elements/elements/PROBooksSection/twig-templates/pro_books_section.js", array('jquery'), '1.0', true);
 
-
-            return $this->twigObj->render("pro_books_section.html.twig", array());
+            $firstBookImage = wp_get_attachment_image_src($atts['first_book_image'], 'full')[0];
+            $secondBookImage = wp_get_attachment_image_src($atts['second_book_image'], 'full')[0];
+            return $this->twigObj->render("pro_books_section.html.twig",
+                [
+                    'first_book_title' => $atts['first_book_title'],
+                    'second_book_title' => $atts['second_book_title'],
+                    'first_book_link' => $atts['first_book_link'],
+                    'second_book_link' => $atts['second_book_link'],
+                    'first_book_image' => $firstBookImage,
+                    'second_book_image' => $secondBookImage,
+                ]
+            );
         }
     }
 }
