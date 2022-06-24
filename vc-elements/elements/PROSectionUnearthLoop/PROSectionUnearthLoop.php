65cf0ed84e9f7af94e00e594c0ad076557117567
marchajjar0 <marchajjar@abiroot.com>
2022-06-24 09:43:47 +0300

    unearth dynamic content

--- vc-elements/elements/PROSectionUnearthLoop/PROSectionUnearthLoop.php
+++ vc-elements/elements/PROSectionUnearthLoop/PROSectionUnearthLoop.php
@@ -19,25 +19,43 @@ if (!class_exists('PROSectionUnearthLoop')) {
 				'category' => __('Profit-isle Elements', 'profit-isle'),
 				'params' => array(
 					array(
-						'heading' => 'Text',
-						'type' => 'textfield',
-						'param_name' => 'button',
-					)
+						'heading' => 'Loop Content',
+						'type' => 'param_group',
+						'param_name' => 'loop_content',
+                        'value'=>'',
+                        'params'=>[
+                            [
+                                'heading'=>'Loop Title',
+                                'type'=>'textfield',
+                                'param_name'=>'loop_title',
+                                'value'=>''
+                            ],
+                            [
+                                'heading'=>'Loop Description',
+                                'type'=>'textarea',
+                                'param_name'=>'loop_description',
+                                'value'=>''
+                            ]
+                        ]
+                    )
 				),
 			));
 		}
 		public function render_shortcode($atts, $content, $tag)
 		{
 			$this->initializeTwigTemplate();
-
 			wp_enqueue_style('pro_section_unearth_loop-style', get_template_directory_uri() .
 				"/vc-elements/elements/PROSectionUnearthLoop/twig-templates/pro_section_unearth_loop.css", array(), '1.0');
 
 			wp_enqueue_script('pro_section_unearth_loop-script', get_template_directory_uri() .
 				"/vc-elements/elements/PROSectionUnearthLoop/twig-templates/pro_section_unearth_loop.js", array('jquery'), '1.0', true);
 
+            $loops =vc_param_group_parse_atts($atts['loop_content']);
 
-			return $this->twigObj->render("pro_section_unearth_loop.html.twig", array());
+
+            return $this->twigObj->render("pro_section_unearth_loop.html.twig", array(
+                "loop_content"=>$loops
+            ));
 		}
 	}
 }
