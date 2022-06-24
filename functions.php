d7cd5dafdcf445933beac44f529c6019c6d0150a
Ayman Abi Aoun <ayman@abiroot.com>
2022-06-24 14:46:30 +0300

    Fix

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_PRI_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('_PRI_VERSION', '1.1.1');
+    define('_PRI_VERSION', '1.1.2');
 }
 
 /**
