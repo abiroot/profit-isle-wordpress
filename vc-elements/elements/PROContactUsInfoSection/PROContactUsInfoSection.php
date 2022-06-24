e72b802af18e4814e81552eb9fd3eaa7ecac0bdb
AkikiCharbel <charbelakiki@abiroot.com>
2022-06-24 15:09:13 +0300

    general info added

--- vc-elements/elements/PROContactUsInfoSection/PROContactUsInfoSection.php
+++ vc-elements/elements/PROContactUsInfoSection/PROContactUsInfoSection.php
@@ -1,43 +1,83 @@
 <?php
 
 if (!class_exists('PROContactUsInfoSection')) {
-	class PROContactUsInfoSection extends VC_Element_Abstract
-	{
+    class PROContactUsInfoSection extends VC_Element_Abstract
+    {
 
-		public function create_shortcode()
-		{
-			// Stop all if VC is not enabled
-			if (!defined('WPB_VC_VERSION')) {
-				return;
-			}
+        public function create_shortcode()
+        {
+            // Stop all if VC is not enabled
+            if (!defined('WPB_VC_VERSION')) {
+                return;
+            }
 
-			// Map blockquote with vc_map()
-			vc_map(array(
-				'name' => __('PROContactUsInfoSection', 'profit-isle'),
-				'base' => 'pro_contact_us_info_section',
-				'description' => __('', 'profit-isle'),
-				'category' => __('Profit-isle Elements', 'profit-isle'),
-				'params' => array(
-					array(
-						'heading' => 'Text',
-						'type' => 'textfield',
-						'param_name' => 'button',
-					)
-				),
-			));
-		}
-		public function render_shortcode($atts, $content, $tag)
-		{
-			$this->initializeTwigTemplate();
+            // Map blockquote with vc_map()
+            vc_map(
+                [
+                    'name' => __('PROContactUsInfoSection', 'profit-isle'),
+                    'base' => 'pro_contact_us_info_section',
+                    'description' => __('', 'profit-isle'),
+                    'category' => __('Profit-isle Elements', 'profit-isle'),
+                    'params' => [
+                        [
+                            'heading' => 'Address',
+                            'type' => 'textfield',
+                            'param_name' => 'address',
+                            'value' => '',
+                        ],
+                        [
+                            'heading' => 'Phone Number',
+                            'type' => 'textfield',
+                            'param_name' => 'phone_number',
+                            'value' => '',
+                        ],
+                        [
+                            'heading' => 'Email',
+                            'type' => 'textfield',
+                            'param_name' => 'email',
+                            'value' => '',
+                        ],
+                        [
+                            'heading' => 'Facebook',
+                            'type' => 'textfield',
+                            'param_name' => 'facebook',
+                            'value' => '',
+                        ],
+                        [
+                            'heading' => 'Linked In',
+                            'type' => 'textfield',
+                            'param_name' => 'linkedin',
+                            'value' => '',
+                        ],
+                        [
+                            'heading' => 'Twitter',
+                            'type' => 'textfield',
+                            'param_name' => 'twitter',
+                            'value' => '',
+                        ],
+                        [
+                            'heading' => 'Youtube',
+                            'type' => 'textfield',
+                            'param_name' => 'youtube',
+                            'value' => '',
+                        ],
+                    ],
+                ]
+            );
+        }
 
-			wp_enqueue_style('pro_contact_us_info_section-style', get_template_directory_uri() .
-				"/vc-elements/elements/PROContactUsInfoSection/twig-templates/pro_contact_us_info_section.css", array(), '1.0');
+        public function render_shortcode($atts, $content, $tag)
+        {
+            $this->initializeTwigTemplate();
 
-			wp_enqueue_script('pro_contact_us_info_section-script', get_template_directory_uri() .
-				"/vc-elements/elements/PROContactUsInfoSection/twig-templates/pro_contact_us_info_section.js", array('jquery'), '1.0', true);
+            wp_enqueue_style('pro_contact_us_info_section-style', get_template_directory_uri() .
+                "/vc-elements/elements/PROContactUsInfoSection/twig-templates/pro_contact_us_info_section.css", array(), '1.0');
+
+            wp_enqueue_script('pro_contact_us_info_section-script', get_template_directory_uri() .
+                "/vc-elements/elements/PROContactUsInfoSection/twig-templates/pro_contact_us_info_section.js", array('jquery'), '1.0', true);
 
 
-			return $this->twigObj->render("pro_contact_us_info_section.html.twig",
+            return $this->twigObj->render("pro_contact_us_info_section.html.twig",
                 [
                     'address' => $atts['address'],
                     'phone_number' => $atts['phone_number'],
@@ -48,6 +88,6 @@ if (!class_exists('PROContactUsInfoSection')) {
                     'youtube' => $atts['youtube'],
                 ]
             );
-		}
-	}
+        }
+    }
 }
