e3315d1d16e17a7c5aeee46c319c7154c3e075d3
Ayman Abi Aoun <ayman@abiroot.com>
2022-07-07 17:32:09 +0300

    Parallax Unearth

--- vc-elements/elements/PROSectionUnearthLoop/twig-templates/pro_section_unearth_loop.js
+++ vc-elements/elements/PROSectionUnearthLoop/twig-templates/pro_section_unearth_loop.js
@@ -1,22 +1,31 @@
-jQuery(window).scroll(function(){
-    setTimeout(function(){
-        var scrollTop     = jQuery(window).scrollTop(),
-            elementOffset = jQuery('.section-unearth-loop').offset().top,
-            distance      = (elementOffset - (scrollTop + 95));
+jQuery('.parallax-section-unearth').scroll(function () {
+    // var scrollTop = jQuery(this).scrollTop(),
+    //     height = jQuery(this).height() * 0.2;
+    //
+    // jQuery('.parallax-section-unearth').css('background-position-y', (height - (scrollTop * 0.2)) + 'px');
 
-        if(distance < 50){
-            jQuery('.section-unearth-loop .scrollable-section').addClass('start-scroll');
-        }else{
-            jQuery('.section-unearth-loop .scrollable-section').removeClass('start-scroll');
-
-        }
-        // console.log("scrollTop", scrollTop)
-        // console.log("elementOffset", elementOffset)
-        // console.log("distance", distance)
-    }, 500);
-    // if(jQuery(window).scrollTop() > jQuery("#div1").offset().top
-    //     && jQuery(window).scrollTop() < jQuery("#div2").offset().top)
-    // {
-    //     doSomething();
-    // }
+    var jQueryel = jQuery('.parallax-section-unearth');
+    jQueryel.on('scroll', function () {
+        var scroll = jQueryel.scrollTop();
+        jQueryel.css({
+            'background-position': '100% ' + (-0.2 * scroll) + 'px'
+        });
+    });
 });
+//
+// // set height of the svg path as constant
+// const svg = document.getElementById("svgPath");
+// const length = svg.getTotalLength();
+//
+// // start positioning of svg drawing
+// svg.style.strokeDasharray = length;
+// // hide svg before scrolling starts
+// svg.style.strokeDashoffset = length;
+// window.addEventListener("scroll", function () {
+//     const scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+//
+//     const draw = length * scrollpercent;
+//
+//     // Reverse the drawing when scroll upwards
+//     svg.style.strokeDashoffset = length - draw;
+// });
---
+++ vc-elements/elements/PROSectionUnearthLoopOriginal/PROSectionUnearthLoop.php
@@ -0,0 +1,62 @@
+<?php
+
+if (!class_exists('PROSectionUnearthLoop')) {
+    class PROSectionUnearthLoop extends VC_Element_Abstract
+    {
+
+        public function create_shortcode()
+        {
+            // Stop all if VC is not enabled
+            if (!defined('WPB_VC_VERSION')) {
+                return;
+            }
+
+            // Map blockquote with vc_map()
+            vc_map(array(
+                'name' => __('PROSectionUnearthLoop', 'profit-isle'),
+                'base' => 'pro_section_unearth_loop',
+                'description' => __('', 'profit-isle'),
+                'category' => __('Profit-isle Elements', 'profit-isle'),
+                'params' => array(
+                    array(
+                        'heading' => 'Loop Content',
+                        'type' => 'param_group',
+                        'param_name' => 'loop_content',
+                        'value' => '',
+                        'params' => [
+                            [
+                                'heading' => 'Loop Title',
+                                'type' => 'textfield',
+                                'param_name' => 'loop_title',
+                                'value' => ''
+                            ],
+                            [
+                                'heading' => 'Loop Description',
+                                'type' => 'textarea',
+                                'param_name' => 'loop_description',
+                                'value' => ''
+                            ]
+                        ]
+                    )
+                ),
+            ));
+        }
+
+        public function render_shortcode($atts, $content, $tag)
+        {
+            $this->initializeTwigTemplate();
+            wp_enqueue_style('pro_section_unearth_loop-style', get_template_directory_uri() .
+                "/vc-elements/elements/PROSectionUnearthLoop/twig-templates/pro_section_unearth_loop.css", array(), '1.0');
+
+            wp_enqueue_script('pro_section_unearth_loop-script', get_template_directory_uri() .
+                "/vc-elements/elements/PROSectionUnearthLoop/twig-templates/pro_section_unearth_loop.js", array('jquery'), '1.0', true);
+
+            $loops = vc_param_group_parse_atts($atts['loop_content']);
+
+
+            return $this->twigObj->render("pro_section_unearth_loop.html.twig", array(
+                "loop_content" => $loops
+            ));
+        }
+    }
+}
