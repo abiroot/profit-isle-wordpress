9f6c60f56a83fae6499cbf0faecd99ac495155cd
marchajjar0 <marchajjar@abiroot.com>
2022-07-08 15:49:51 +0300

    messy data

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_PRI_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('_PRI_VERSION', '1.2.0');
+    define('_PRI_VERSION', '1.2.1');
 }
 
 /**
